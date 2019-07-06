059d378f2c0d6ca68abe8fdb547296b21b9cd31d
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2019-07-06 20:07:23 +0300

    Промежуточный результат

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {serveClient: true});
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
 let users = [];
-let connections = [];
-
 
 app.use(express.static(__dirname));
-//указываем корневую деректорию
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  
-  socket.emit('connected', 'a user connected')
+let numUsers = 0;
 
-  socket.join('all')
-  socket.on('msg', function (msg) {
-    const obj = {
-      date: new Date(),
-      content: msg,
-      username: socket.id
-    }
-    socket.emit('message', obj)
-    socket.to('all').emit('message', obj)
-  });
-  socket.on('receiveHistory', ()=>{
-    //localStorage
+io.on('connection', function (socket) {
+  let addedUser = false;
+
+  socket.on('add user', (userInfo) => {
+    if (addedUser) return;
+
+    socket.userInfo = userInfo;
+    users.push(socket.userInfo);
+
+    ++numUsers;
+    addedUser = true;
+
+    socket.emit('login', { 
+      userInfo: socket.userInfo,
+      numUsers: numUsers,
+      users: users 
+    })
+
+    socket.broadcast.emit('user joined', {
+      userInfo: socket.userInfo,
+      numUsers: numUsers,
+      users: users
+    })
   })
 
+  socket.on('load image', (image) => {
+    socket.userInfo.image = image;
+    io.emit('refresh image', users);
+  })
+
+  socket.on('new message', (message) => {
+    io.emit('new message', {
+      date: getDate(),
+      content: message,
+      username: socket.userInfo.name,
+      image: socket.userInfo.image
+    })
+  })
+
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      users.splice(users.indexOf(socket.userInfo), 1);
+
+      socket.broadcast.emit('user left', {
+        userInfo: socket.userInfo,
+        numUsers: numUsers,
+        users: users
+      })
+    }
+  })
+
+  socket.on('receiveHistory', ()=>{
+    // localStorage
+  })
 });
+
 // io.on('send mess', (data) => {
 //   io.socket.emit('new mess', messageData);
 // });
 //localhost:3000
-http.listen(3000, function () {
-  console.log('listening on *:3000');
-});
+
+function getDate() {
+  const date    = new Date();
+  const hours   = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+  const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+
+  return `${hours}:${minutes}`;
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
 let name = document.querySelector('#name'),
     nickname = document.querySelector('#nickname'),
-    authBtn = document.querySelector('#authBtn'),
-    sendBtn = document.querySelector('#sendBtn'),
+    authForm = document.querySelector('#authPopup'),
+    sendMessageForm = document.querySelector('.message__form'),
     messageText = document.querySelector('#messageText'),
     messages = document.querySelector('#messages');
     messageContainer = document.querySelector('.message__container'),
-    usersList = document.querySelector('#usersList'),
     authPopup = document.querySelector('#authPopup'),
     loadPhoto = document.querySelector('#loadPhoto'),
     fileLoadPopup = document.querySelector('#fileLoadPopup'),
+    dropArea = document.getElementById('drop-area'),
     cancel = document.querySelector('#cancel'),
     sendPhoto = document.querySelector('#sendPhoto'),
     container = document.querySelector('.container'),
@@ -20,43 +19,170 @@ let name = document.querySelector('#name'),
     templateOfMessage = document.querySelector('#messageList').textContent,
     templateOfUsers = document.querySelector('#listOfUsers').textContent,
     renderUsers = Handlebars.compile(templateOfUsers),
-    renderMessages = Handlebars.compile(templateOfMessage),
-    users = [],
-    messagesListOnPage = [],
+    renderMessage = Handlebars.compile(templateOfMessage),
+    connected = false,
     socket = io.connect('http://localhost:3000/');
-    socket.on('connected', function (msg) {
-      console.log(msg);
-      //socket.emit('receiveHistory');
-  });
 
-  socket.on('message', addMessage);
+// -----------------------------------------------------------------
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults)
+})
 
-    sendBtn.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      let messageData = messageText.value.trim();
-      let messageInfo = {
-       // name: name.value.trim() + ':',
-        message: messageData
-      };
-
-      
-      if(messageData.length == 0){
-        
-        messageText.classList.add('message__form_input-error');
-      }
-      socket.emit('msg', messageData);
-      messageText.value = '';
-        
-    });
-
-function addMessage(message){
-  
-  messagesListOnPage.push(message);
-  let messagesList = renderMessages(messagesListOnPage);
-        messages.innerHTML = messagesList;
-        messageContainer.scrollTop = messageContainer.scrollHeight;
+function preventDefaults (evt) {
+  evt.preventDefault()
+  evt.stopPropagation()
 }
-  
 
-});
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight)
+})
+
+function highlight(evt) {
+  dropArea.classList.add('highlight')
+}
+function unhighlight(evt) {
+  dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop)
+let globalFile;
+function handleDrop(evt) {
+  evt.preventDefault();
+  evt.stopPropagation();
+  let dt = evt.dataTransfer
+  let file = dt.files[0]
+  globalFile = file;
+  handleFile(file)
+}
+
+function handleFile(file) {
+  previewFile(file)
+  sendPhoto.addEventListener('click', () => {
+    uploadFile(file);
+  })
+}
+
+function uploadFile(file) {
+  fileLoadPopup.style.display = 'none';
+
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    document.querySelector('#avatarImage').src = reader.result;
+    socket.emit('load image', reader.result)
+  }
+}
+
+function previewFile(file) {
+  document.getElementById('preview').innerHTML = '';
+
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = function() {
+    let img = document.createElement('img')
+    img.src = reader.result
+    document.getElementById('preview').appendChild(img)
+  }
+}
+
+// -----------------------------------------------------------------
+function onAuthFormSubmit(evt) {
+  evt.preventDefault();
+
+  socket.emit('add user', {
+    name: name.value.trim(),
+    nickname: nickname.value.trim()
+  })
+
+  authPopup.style.display = 'none'
+}
+
+function onSendMessageFormSubmit(evt) {
+  evt.preventDefault();
+
+  let messageData = messageText.value.trim();
+  socket.emit('new message', messageData);
+  messageText.value = '';
+}
+
+function onLoadPhotoClick(evt) {
+  evt.preventDefault();
+
+  fileLoadPopup.style.display = 'block';
+}
+
+function onCancelBtnClick(evt) {
+  evt.preventDefault();
+
+  fileLoadPopup.style.display = 'none';
+}
+
+function addMessage(messageInfo){
+  if (!connected) return;
+
+  const messageDiv = document.createElement('div')
+  messageDiv.innerHTML = renderMessage(messageInfo);
+  messageDiv.classList.add('message__item')
+  messages.appendChild(messageDiv);
+  messageContainer.scrollTop = messageContainer.scrollHeight;
+}
+
+function showUsers(users) {
+  usersList.innerHTML = renderUsers(users);
+}
+
+authForm.addEventListener('submit', onAuthFormSubmit)
+sendMessageForm.addEventListener('submit', onSendMessageFormSubmit)
+loadPhoto.addEventListener('click', onLoadPhotoClick)
+cancel.addEventListener('click', onCancelBtnClick)
+
+socket.on('login', (data) => {
+  connected = true;
+  document.querySelector('.users__block_name').textContent = data.userInfo.name;
+  showUsers(data.users);
+
+  let div = document.createElement('div');
+  div.innerHTML = '<b>You are WELCOME!</b>'
+
+  messages.appendChild(div)
+})
+
+socket.on('user joined', (data) => {
+  if (!connected) return;
+
+  showUsers(data.users);
+
+  let div = document.createElement('div');
+  div.innerHTML = `<b>${data.userInfo.name}</b> has just joined the chat`
+
+  messages.appendChild(div)
+})
+
+socket.on('refresh image', (users) => {
+  showUsers(users);
+})
+
+socket.on('user left', (data) => {
+  if (!connected) return;
+
+  showUsers(data.users);
+
+  let div = document.createElement('div');
+  div.innerHTML = `<b>${data.userInfo.name}</b> has just left the chat`
+
+  messages.appendChild(div)
+})
+
+socket.on('new message', addMessage);
+
+function getDate() {
+  const date    = new Date();
+  const hours   = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+  const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+
+  return `${hours}:${minutes}`;
+}
